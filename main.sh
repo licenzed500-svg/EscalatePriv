20e5258ec1e2893da34e203a73ed2eb48c844d85
Maksim Merzhew <79693216+licenzed500-svg@users.noreply.github.com>
2024-12-07 14:18:15 -0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -4,17 +4,14 @@
 find_libraries() {
     echo "Поиск библиотек и их версий в системе..."
     for lib in $(find / -type f \( -name "*.so" -o -name "*.so.*" \) 2>/dev/null); do
-        echo "Библиотека: $lib"
         version=$(strings $lib 2>/dev/null | grep -E "^[0-9]+\.[0-9]+(\.[0-9]+)?$")
         if [[ ! -z $version ]]; then
+            echo "Библиотека: $lib"
             echo "Версия: $version"
-        else
-            echo "Версия не найдена"
+            echo "-----------------------"
         fi
-        echo "-----------------------"
     done
 }
 
 # Запуск функции
 find_libraries
-
